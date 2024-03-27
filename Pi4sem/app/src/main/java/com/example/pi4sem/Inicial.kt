ecd0251c85f5b54a401dbc115ae2e5dfb466fcb5
THAIS RODRIGUES ANDRADE <thais.randrade5@senacsp.edu.br>
2024-03-27 20:50:01 -0300

    isso

--- Pi4sem/app/src/main/java/com/example/pi4sem/Inicial.kt
+++ Pi4sem/app/src/main/java/com/example/pi4sem/Inicial.kt
@@ -3,11 +3,9 @@ package com.example.pi4sem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Inicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_inicial)
     }
 }
