a8ceeace52f43fc05f576d51a13e191f37e32e6e
THAIS RODRIGUES ANDRADE <thais.randrade5@senacsp.edu.br>
2024-03-27 21:22:32 -0300

    isso

--- Pi4sem/app/src/main/java/com/example/pi4sem/MainActivity.kt
+++ Pi4sem/app/src/main/java/com/example/pi4sem/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.pi4sem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+        val inputEmail = findViewById<EditText>(R.id.email)
+        val inputSenha = findViewById<EditText>(R.id.senha)
+
+        btnEntrar.setOnClickListener() {
+            var Email = inputEmail.getText().toString()
+
+            val intent = Intent(this, tela_produtos::class.java)
+            intent.putExtra("usuario", Email)
+            startActivity(intent)
+        }
     }
+
 }
