43fbf1df5c788a74a845dc7c186f5f7bc380285a
THAIS RODRIGUES ANDRADE <thais.randrade5@senacsp.edu.br>
2024-05-10 21:56:29 -0300

    okkk

--- Pi4sem/app/src/main/java/com/example/pi4sem/Carrinho.kt
+++ Pi4sem/app/src/main/java/com/example/pi4sem/Carrinho.kt
@@ -1,9 +1,10 @@
 package com.example.pi4sem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -17,6 +18,8 @@ class Carrinho : AppCompatActivity() {
     private lateinit var totalTextView: TextView
     private lateinit var goToPaymentButton: Button
     private var total: Double = 0.0
+    private lateinit var cartAdapter: CartAdapter
+    private var items: MutableList<Produto> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,25 +33,28 @@ class Carrinho : AppCompatActivity() {
         fetchCartItems()
 
         goToPaymentButton.setOnClickListener {
-            // Ir para tela de pagamento enviando os dados
+            val intent = Intent(this, PaymentActivity::class.java).apply {
+                putExtra("TOTAL", total) // Passe o total como Double
+                putExtra("USER", 271) // ID do usuÃ¡rio, idealmente obtido de forma segura
+                putParcelableArrayListExtra("PRODUCT_LIST", ArrayList(items))
+            }
+            startActivity(intent)
         }
     }
 
     private fun fetchCartItems() {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://951c7ccc-dfe0-45e1-9cb8-a93dbb2f287f-00-2vz8o7pqwf4y2.janeway.repl.co/")
+            .baseUrl("https://9038ecf2-2975-4974-8922-924bf7ea6920-00-im976vx84po1.kirk.repl.co/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val api = retrofit.create(CartApiService::class.java)
-        api.getCartItems(userId = 271).enqueue(object : Callback<List<Produto>> {
+        api.getCartItems(userId = 271).enqueue(object : Callback<List<Produto>> { // RECUPERE A INFORMAÃ‡ÃƒO DO ID DO USUÃRIO ATRAVÃ‰S DE SHAREDPREFERENCES
             override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
                 if (response.isSuccessful) {
-                    val cartItems = response.body()?.toMutableList() ?: mutableListOf()
-                    recyclerView.adapter = CartAdapter(cartItems, this@Carrinho) {
-                        total = cartItems.sumOf { it.produtoPreco.toDouble() * it.quantidadeDisponivel }
-                        totalTextView.text = "Total: R$${String.format("%.2f", total)}"
-                    }
+                    items = response.body()?.toMutableList() ?: mutableListOf()
+                    setupAdapter()
+                    updateTotal()  // Atualiza o total apÃ³s carregar os itens
                 }
             }
 
@@ -56,5 +62,17 @@ class Carrinho : AppCompatActivity() {
                 // Tratamento de falhas
             }
         })
+    }
+
+    private fun setupAdapter() {
+        cartAdapter = CartAdapter(items, this) { updateTotal() }
+        recyclerView.adapter = cartAdapter
+    }
+
+    fun updateTotal() {
+        total = items.sumOf { it.produtoPreco * it.quantidadeDisponivel }
+        runOnUiThread {
+            totalTextView.text = "Total: R$${String.format("%.2f", total)}"
+        }
     }
 }
---
+++ Pi4sem/app/src/main/java/com/example/pi4sem/ResponseCompra.kt
@@ -0,0 +1,7 @@
+package com.example.pi4sem
+
+data class ResponseCompra(
+    val status: String,
+    val code: Int,
+    val message: String
+)
